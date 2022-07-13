e1b4110f4a06e8093e30c511114cfd7f27bcafe6
plche <che.percy.l@gmail.com>
2022-07-13 12:11:28 -0500

    Solución Reto 1

---
+++ utilityHost.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+# Programa para resolver el Reto 1
+# Autor: Percy Che
+
+option=A
+result=$((5+2))
+
+echo "El valor de la variable 'option' es: $option"
+echo "El valor de la variable 'result' es: $result"
