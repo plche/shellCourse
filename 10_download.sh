d20ac1496f59033a66322284d02894f31880ed59
plche <che.percy.l@gmail.com>
2022-07-13 12:22:40 -0500

    Descargar de Internet

---
+++ 10_download.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+# Para borrar una linea entera en VIM ubicarse sobre la línea y presionar 'dd'
+# Programa para ejemplificar el uso de la descarga de información desde internet utilizando el comando wget
+# Autor: Marco Toscano - @martosfre
+
+echo "Descargar información de Internet"
+wget https://downloads.apache.org/tomcat/tomcat-8/v8.5.81/bin/apache-tomcat-8.5.81.zip
